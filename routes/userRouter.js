36f225aa2e7f3eb6e9c35a8ec555d11253447dc0
Ribot Alexis <alexisribot10@gmail.com>
2020-07-13 21:36:27 +0200

    login, register, error handling, jwt set up

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const User = require ("../models/userModel");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+const cookieParser = require("cookie-parser");
 
 router.post("/register", async (req, res) => {
     try {
@@ -71,7 +73,6 @@ router.post("/login", async (req, res) =>  {
                 id: user._id,
                 name: user.name,
                 surname: user.surname,
-                email: user.email
             },
         })
         console.log(token);
@@ -80,4 +81,25 @@ router.post("/login", async (req, res) =>  {
     }
 })
 
+router.post("/tokenIsValid", async (req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.json(false);
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) return res.json(false);
+
+        const user = await User.findById(verified.id);
+        if (!user) return res.json(false);
+
+        return res.json(true);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.get("/", auth, async (req, res) =>  {
+    const user = await User.findById(req.user);
+    res.json(user);
+})
 module.exports = router;
