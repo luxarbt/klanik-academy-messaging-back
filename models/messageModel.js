dc390ddfe2d51e40b1b5e533f724d4200e19d894
Ribot Alexis <alexisribot10@gmail.com>
2020-09-08 15:09:13 +0200

    Encrypt data

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const messageSchema = new mongoose.Schema({
   sender: {
@@ -17,6 +18,15 @@ const messageSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Conversation",
   },
+});
+
+const encKey = process.env.ENCKEY;
+const sigKey = process.env.SIGKEY;
+
+messageSchema.plugin(encrypt, {
+  encryptionKey: encKey,
+  signingKey: sigKey,
+  encryptedFields: ["sender", "receiver", "message"],
 });
 
 // eslint-disable-next-line no-undef
--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 5 },
   name: { type: String, required: true },
   surname: { type: String, required: true },
+});
+
+const encKey = process.env.ENCKEY;
+const sigKey = process.env.SIGKEY;
+
+userSchema.plugin(encrypt, {
+  encryptionKey: encKey,
+  signingKey: sigKey,
+  encryptedFields: ["name", "surname"],
 });
 
 // eslint-disable-next-line no-undef
